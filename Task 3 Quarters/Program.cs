9bb0f98eaf62309087daa7eaaaf94aabd3b69784
Dunin <snayplx@mail.ru>
2023-01-21 21:18:52 +0300

    Home Work 2

---
+++ Task 3 Quarters/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+
+Console.WriteLine("Введите номер четверти");
+
+int numbers = int.Parse (Console.ReadLine());
+
+
+if (numbers < 1 || numbers > 4)
+{
+    Console.WriteLine("Вы не ввели правильный номер четверти(Введите от 1 до 4)");
+}
+
+else if (numbers == 1)
+{
+    Console.WriteLine("Возможные координаты в 1 четверти: Х > 0, Y > 0");
+}
+else if (numbers == 2)
+{
+    Console.WriteLine("Возможные координаты в 2 четверти: Х < 0, Y > 0");
+}
+
+else if (numbers == 3)
+{
+    Console.WriteLine("Возможные координаты в 3 четверти: Х < 0, Y < 0");
+}
+
+else if (numbers == 4)
+{
+    Console.WriteLine("Возможные координаты в 4 четверти: Х > 0, Y < 0");
+}
+
+
---
+++ Task 4 2d and 3d/Program.cs
@@ -0,0 +1,83 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+
+Console.WriteLine("Введите цифру 1 для рассчета расстояния между точками в 3D или цифру 2 для рассчета в 2D");
+int choice = int.Parse (Console.ReadLine());
+
+if (choice == 1)
+{
+Console.WriteLine("Введите у первого числа координаты по оси Х");
+int X1 = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Введите у первого числа координаты по оси У");
+int Y1 = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Введите у первого числа координаты по оси Z");
+int Z1 = int.Parse (Console.ReadLine());
+
+
+Console.WriteLine("Введите у второго числа координаты по оси Х");
+int X2 = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Введите у второго числа координаты по оси У");
+int Y2 = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Введите у второго числа координаты по оси Z");
+int Z2 = int.Parse (Console.ReadLine());
+
+
+int deltaX = X1 - X2;
+
+int deltaY = Y1 - Y2;
+
+int deltaZ = Z1 - Z2;
+
+int deltaE = (deltaX*deltaX + deltaY*deltaY + deltaZ*deltaZ);
+
+double delta3D;
+
+delta3D = Math.Pow(deltaE, 0.5);
+
+
+
+Console.WriteLine($"Расстояние между одной и второй точкой в 3D равна {delta3D}");
+}
+
+
+
+else
+{
+    
+Console.WriteLine("Введите у первого числа координаты по оси Х");
+int X1 = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Введите у первого числа координаты по оси У");
+int Y1 = int.Parse (Console.ReadLine());
+
+
+Console.WriteLine("Введите у второго числа координаты по оси Х");
+int X2 = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Введите у второго числа координаты по оси У");
+int Y2 = int.Parse (Console.ReadLine());
+
+
+
+
+int deltaX = X1 - X2;
+
+int deltaY = Y1 - Y2;
+
+
+
+int deltaE = (deltaX*deltaX + deltaY*deltaY );
+
+double delta3D;
+
+delta3D = Math.Pow(deltaE, 0.5);
+
+
+
+Console.WriteLine($"Расстояние между одной и второй точкой в 3D равна {delta3D}");
+
+
+}
